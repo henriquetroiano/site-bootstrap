b1ca058c19a5748cbff394473d32f163466e3416
henriquetroiano <henriquetroiano@hotmail.com>
2020-08-09 21:38:54 -0300

    final version

--- js/dimensions.js
+++ js/dimensions.js
@@ -3,7 +3,9 @@ export default function dimensions() {
     const windowHeight = window.innerHeight;
 
     const condominioSection = document.querySelector('#condominio');
+    const faqSection = document.querySelector('#faq');
     if(windowHeight >= 780) {
         condominioSection.style.height = windowHeight - navbarHeight + "px";
+        faqSection.style.height = windowHeight - navbarHeight + "px";
     }
 }
